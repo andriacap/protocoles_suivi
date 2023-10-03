b4afb67c3590ff8fee0d59cc7fad52471c1238bf
Andria Capai <andria_capai@natural-solutions.eu>
2023-10-03 18:19:28 +0200

    Change synthese.sql
    Adapt synthese to protocol
    
    Reviewed-by: andriacap

--- Odonates/synthese.sql
+++ Odonates/synthese.sql
@@ -36,12 +36,13 @@ AS WITH source AS (
     ref_nomenclatures.get_id_nomenclature('METH_OBS'::character varying, '20'::character varying) AS id_nomenclature_obs_meth, 
     nullif(json_extract_path(oc.data::json,'id_nomenclature_stade')::text,'null')::integer AS id_nomenclature_life_stage,
     nullif(json_extract_path(oc.data::json,'id_nomenclature_sex')::text,'null')::integer AS id_nomenclature_sex,
-    ref_nomenclatures.get_id_nomenclature('OBJ_DENBR'::character varying, 'IND'::character varying) AS id_nomenclature_obj_count,
-    nullif(json_extract_path(oc.data::json,'id_nomenclature_typ_denbr')::text, 'null')::integer AS id_nomenclature_type_count,
-    ref_nomenclatures.get_id_nomenclature('STATUT_OBS'::character varying, 'Pr'::character varying) AS id_nomenclature_observation_status,
-    ref_nomenclatures.get_id_nomenclature('ETAT_BIO'::character varying, '1'::character varying) as id_nomenclature_bio_condition,
-    ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE'::character varying, 'Te'::character varying) AS id_nomenclature_source_status,
-    ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO'::character varying, '1'::character varying) AS id_nomenclature_info_geo_type,
+    nullif(json_extract_path(oc.data::json,'id_nomenclature_etat_bio')::text,'null')::integer AS id_nomenclature_etat_bio,
+    -- ref_nomenclatures.get_id_nomenclature('OBJ_DENBR'::character varying, 'IND'::character varying) AS id_nomenclature_obj_count,
+    -- nullif(json_extract_path(oc.data::json,'id_nomenclature_typ_denbr')::text, 'null')::integer AS id_nomenclature_type_count,
+    -- ref_nomenclatures.get_id_nomenclature('STATUT_OBS'::character varying, 'Pr'::character varying) AS id_nomenclature_observation_status,
+    -- ref_nomenclatures.get_id_nomenclature('ETAT_BIO'::character varying, '1'::character varying) as id_nomenclature_bio_condition,
+    -- ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE'::character varying, 'Te'::character varying) AS id_nomenclature_source_status,
+    -- ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO'::character varying, '1'::character varying) AS id_nomenclature_info_geo_type,
     nullif(((oc.data::json #> '{count_min}'::text[])::text),'null')::integer AS count_min,
     nullif(((oc.data::json #> '{count_max}'::text[])::text),'null')::integer AS count_max,
     o.id_observation,
@@ -64,27 +65,33 @@ AS WITH source AS (
     v.id_base_site,
     v.id_base_visit, 
     json_build_object(
-    	'aire_etude', tsg.sites_group_name,
         'nom_site', s.base_site_name,
-        'milieu_aquatique', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(sc.data::json,'milieu_aquatique')::text,'null')::integer, 'fr'),
-        'variation_eau', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(sc.data::json,'variation_eau')::text,'null')::integer, 'fr'),
-        'courant',  ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(sc.data::json,'courant')::text,'null')::integer, 'fr'),
-    	'num_passage', json_extract_path(vc.data::json,'num_passage')::text, 
-    	'accessibilite', (vc.data::json #> '{accessibility}'::text[]),
-    	'pluviosite', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'pluviosite')::text,'null')::integer, 'fr'),
-    	'couverture_nuageuse', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'couverture_nuageuse')::text,'null')::integer, 'fr'),
-    	'vent', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'vent')::text,'null')::integer, 'fr'),
-    	'turbidite', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'turbidite')::text,'null')::integer, 'fr'),
-    	'vegetation_aquatique_principale', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'vegetation_aquatique_principale')::text,'null')::integer, 'fr'),
-    	'rives', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'rives')::text,'null')::integer, 'fr'),
-    	'habitat_terrestre_environnant', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'habitat_terrestre_environnant')::text,'null')::integer, 'fr'),
-    	'activite_humaine', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(vc.data::json,'activite_humaine')::text,'null')::integer, 'fr')
-    	) as additional_data
+        'owner_site',json_extract_path(sc.data::json,'owner')::text, 
+        'proprietepzh',json_extract_path(sc.data::json,'proprietepzh')::text, 
+        'id_nomenclature_type_site', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(sc.data::json,'id_nomenclature_type_site')::text,'null')::integer, 'fr'),
+        'id_nomenclature_prospection', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(sc.data::json,'id_nomenclature_prospection')::text,'null')::integer, 'fr'),
+        'passage', json_extract_path(vc.data::json,'passage')::text, 
+    	'periode', json_extract_path(vc.data::json,'periode')::text, 
+      'tempAir', json_extract_path(vc.data::json,'tempAir')::text, 
+      'humidite', json_extract_path(vc.data::json,'humidite')::text, 
+      'pluviosite', json_extract_path(vc.data::json,'pluviosite')::text, 
+      'couvertureNuageuse', json_extract_path(vc.data::json,'couvertureNuageuse')::text,
+      'vent', json_extract_path(vc.data::json,'vent')::text,  
+      'pertubations', json_extract_path(vc.data::json,'pertubations')::text,  
+      'comment_visit', json_extract_path(vc.data::json,'comment_visit')::text,
+      'etat_bio', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(oc.data::json,'id_nomenclature_etat_bio')::text,'null')::integer, 'fr'),
+      'stade_de_vie', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(oc.data::json,'id_nomenclature_stade')::text,'null')::integer, 'fr'),
+  'sexe_majoritaire', ref_nomenclatures.get_nomenclature_label(nullif(json_extract_path(oc.data::json,'id_nomenclature_sex')::text,'null')::integer, 'fr'),
+  'comportement', json_extract_path(oc.data::json,'comportement')::text,
+   'count_tranche', json_extract_path(oc.data::json,'count_tranche')::text,
+   'count_exact', json_extract_path(oc.data::json,'count_exact')::text,
+   'count_average', json_extract_path(oc.data::json,'count_average')::text,
+  'comment_obs', json_extract_path(oc.data::json,'comments')::text
+  ) as additional_data
    FROM gn_monitoring.t_base_visits v
    	 JOIN gn_monitoring.t_visit_complements vc on v.id_base_visit = vc.id_base_visit 
      JOIN gn_monitoring.t_base_sites s ON s.id_base_site = v.id_base_site
      JOIN gn_monitoring.t_site_complements sc on sc.id_base_site = s.id_base_site
-     JOIN gn_monitoring.t_sites_groups tsg ON sc.id_sites_group = tsg.id_sites_group
      JOIN gn_commons.t_modules m ON m.id_module = v.id_module
      JOIN gn_monitoring.t_observations o ON o.id_base_visit = v.id_base_visit
      JOIN gn_monitoring.t_observation_complements oc ON oc.id_observation = o.id_observation
